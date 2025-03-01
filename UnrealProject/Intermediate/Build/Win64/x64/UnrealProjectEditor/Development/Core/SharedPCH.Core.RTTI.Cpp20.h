6a654d5eeaa99b9247ffe7aaa942ecc814b9196c
hammone2 <hammone2@my.erau.edu>
2025-03-01 15:28:14 -0700

    Got Unreal working on my pc

--- UnrealProject/Intermediate/Build/Win64/x64/UnrealProjectEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
+++ UnrealProject/Intermediate/Build/Win64/x64/UnrealProjectEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "F:/CS 225/CS225-Unreal/UnrealProject/Intermediate/Build/Win64/x64/UnrealProjectEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
+#include "C:/Users/Ethan/Documents/GitHub/CS225-Unreal/UnrealProject/Intermediate/Build/Win64/x64/UnrealProjectEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
 #include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ UnrealProject/Intermediate/Build/Win64/x64/UnrealProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h.old
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "F:/CS 225/CS225-Unreal/UnrealProject/Intermediate/Build/Win64/x64/UnrealProjectEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
